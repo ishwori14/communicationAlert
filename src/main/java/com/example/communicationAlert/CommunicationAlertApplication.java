9fe28784b886f9556d2a4a47413a44315ee026ce
Isuph Shrestha <shresthaisuph2@gmail.com>
2024-02-05 13:08:16 -0800

    init

---
+++ src/main/java/com/example/communicationAlert/CommunicationAlertApplication.java
@@ -0,0 +1,13 @@
+package com.example.communicationAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicationAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunicationAlertApplication.class, args);
+	}
+
+}
